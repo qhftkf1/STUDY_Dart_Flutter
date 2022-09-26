80d13f97f5dbf90166485dc8c831106e06238afc
unknown <qhftkf1@naver.com>
2022-09-27 00:18:13 +0900

    study: Drawer Study drawer, Listview, ListTile and so on. (20220927)

---
+++ android/app/src/main/kotlin/com/example/hellowworld/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hellowworld
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title :Text('Appbar icon menu'),
+        centerTitle: true,
+        elevation: 0.0,
+
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: (){
+              print('shopping button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              print('search button is clicked');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/kk-logo-blue.png'),
+                backgroundColor: Colors.white,
+            ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/kk-logo-blue.png'),
+                  backgroundColor: Colors.white,
+                ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage('assets/kk-logo-blue.png'),
+                //   backgroundColor: Colors.white,
+                // )
+              ],
+              accountEmail: Text('qhftkf1@naver.com'),
+              accountName: Text('Song'),
+              onDetailsPressed: (){
+                print("test");
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+              color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: (){
+                print("Home is clicked");
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text('Settings'),
+              onTap: (){
+                print("Settings is clicked");
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: (){
+                print("Q&A is clicked");
+              },
+              trailing: Icon(Icons.add),
+            )
+          ],
+        ),
+      ),
+    );
+
+  }
+}
